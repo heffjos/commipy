3be5ca2543983863e0c981c84eed9321fa245cc3
Joe Heffernan <heffjos@umich.edu>
2018-05-25 20:29:20 -0500

    Messing with code creating cifri file.

--- create_abnormal_dscalar.py
+++ create_abnormal_dscalar.py
@@ -13,17 +13,21 @@ wrong_cortex_right_indices = np.random.choice(ch.HCP_32K_FS_R,
 wrong_cortex_right_indices.sort()
 
 scalar_info = ch.NamedMapInfo(name="random", meta={})
-left_cortex = ch.MapInfo(brain_structure=ch.Structure.CORTEX_LEFT,
-    indices=cortex_left_indices, model_type=ch.ModelType.SURFACE,
-    surface_number_of_vertices=ch.HCP_32K_FS_L)
 right_cortex = ch.MapInfo(brain_structure=ch.Structure.CORTEX_RIGHT,
     indices=wrong_cortex_right_indices, model_type=ch.ModelType.SURFACE,
     surface_number_of_vertices=ch.HCP_32K_FS_R)
+left_cortex = ch.MapInfo(brain_structure=ch.Structure.CORTEX_LEFT,
+    indices=cortex_left_indices, model_type=ch.ModelType.SURFACE,
+    surface_number_of_vertices=ch.HCP_32K_FS_L)
 
 scalar_map = ch.create_scalar_map((0,), [ch.NamedMapInfo(name="wrong", meta={})])
-brain_models = ch.create_brain_models([left_cortex, right_cortex])
+brain_models = ch.create_brain_models([right_cortex, left_cortex, left_cortex])
 geometry_map = ch.create_geometry_map((1,), brain_models)
-data = np.random.uniform(size=len(cortex_left_indices) + len(wrong_cortex_right_indices))
+data = np.random.uniform(
+    size=(1, len(cortex_left_indices) + len(wrong_cortex_right_indices)))
+dscalar_img = ch.create_dscalar(scalar_map, geometry_map, data)
+
+ci.save(dscalar_img, out_file)
 
 
      
